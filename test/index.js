96c07e5f698d74e2fc9eb9ca605e78ba14d2e997
Jacky Tang <jackytck@gmail.com>
2016-05-17 14:33:02 +0800

    + add node-notifier for notification

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 var aliOSS = require('../dst')
+var notifier = require('node-notifier')
 
 var client = new aliOSS({
   accessKeyId: process.env.accessKeyId,
@@ -11,14 +12,28 @@ var client = new aliOSS({
 
 console.log('Begin test...')
 
-// client.syncDir(`${process.env.dataWeb}/${process.env.testDir2}`, process.env.testDir2)
-//   .then(result => {
-//     console.log('Done syncDir')
-//   })
-//   .catch(console.error)
-
-client.deleteDir(process.env.testDir2)
+client.syncDir(`${process.env.dataWeb2}/${process.env.testDir2}`, process.env.testDir2)
   .then(result => {
-    console.log(result)
+    console.log('done syncDir')
+    // console.log(result)
+    notifier.notify({
+      title: 'ali-oss-extra',
+      message: 'Done syncDir!'
+    })
   })
-  .catch(console.error)
+  .catch(err => {
+    console.error('Error', err)
+    notifier.notify({
+      title: 'ali-oss-extra',
+      message: 'Error!'
+    })
+  })
+
+// client.deleteDir(process.env.hkust)
+//   .then(result => {
+//     console.log(`Deleted ${result.length} files`)
+//   })
+//   .catch(err => {
+//     console.error('error')
+//     console.error(err)
+//   })
